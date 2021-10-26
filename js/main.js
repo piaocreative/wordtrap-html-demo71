e7af6576ed7b3357a9ffdbb66340d3f6f846a5ea
piaocreative <johnpiao171019@gmail.com>
2021-10-26 21:51:36 +0800

    1026

--- js/main.js
+++ js/main.js
@@ -68,6 +68,40 @@ $( document ).ready( function() {
     ]
   });
 
+  $( '.appointment').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+  });
+
   $('.testimonials').slick({
     dots: false,
     infinite: true,
